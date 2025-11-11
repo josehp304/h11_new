f6e6f7323968086d97ac091f3ef3e4e9b28e805a
sreyas <sreyassatheesh755@gmail.com>
2025-11-11 17:34:08 +0530

    feat: enhance MapComponent with additional global locations and style updates
    - Added multiple new locations including major cities worldwide to the MapComponent.
    - Adjusted map projection scale for better visibility.
    - Updated circle fill color and font weight for improved aesthetics.

--- terrene/src/components/Map/MapComponent.jsx
+++ terrene/src/components/Map/MapComponent.jsx
@@ -15,6 +15,27 @@ const locations = [
     name: "Coimbatore",
     coordinates: [77.015083, 11.094444],
   },
+  { name: "New York", coordinates: [-74.006, 40.7128] },
+  { name: "Los Angeles", coordinates: [-118.2437, 34.0522] },
+  { name: "Toronto", coordinates: [-79.3832, 43.6532] },
+  { name: "Mexico City", coordinates: [-99.1332, 19.4326] },
+  { name: "Buenos Aires", coordinates: [-58.3816, -34.6037] },
+  { name: "Sao Paulo", coordinates: [-46.6333, -23.5505] },
+  { name: "London", coordinates: [-0.1276, 51.5074] },
+  { name: "Paris", coordinates: [2.3522, 48.8566] },
+  { name: "Berlin", coordinates: [13.405, 52.52] },
+  { name: "Madrid", coordinates: [-3.7038, 40.4168] },
+  { name: "Rome", coordinates: [12.4964, 41.9028] },
+  { name: "Cairo", coordinates: [31.2357, 30.0444] },
+  { name: "Nairobi", coordinates: [36.8219, -1.2921] },
+  { name: "Johannesburg", coordinates: [28.0473, -26.2041] },
+  { name: "Istanbul", coordinates: [28.9784, 41.0082] },
+  { name: "Dubai", coordinates: [55.2708, 25.2048] },
+  { name: "Singapore", coordinates: [103.8198, 1.3521] },
+  { name: "Tokyo", coordinates: [139.6917, 35.6895] },
+  { name: "Seoul", coordinates: [126.978, 37.5665] },
+  { name: "Sydney", coordinates: [151.2093, -33.8688] },
+  { name: "Auckland", coordinates: [174.7633, -36.8485] },
 ];
 
 export default function MapChart() {
@@ -46,8 +67,8 @@ export default function MapChart() {
           <ComposableMap
             projection="geoMercator"
             projectionConfig={{
-              scale: 750,
-              center: [73, 21],
+              scale: 200,
+              // center: [73, 21],
             }}
             style={{
               width: "100%",
@@ -96,7 +117,7 @@ export default function MapChart() {
               >
                 <circle
                   r={6}
-                  fill="var(--base-250)"
+                  fill="#ff8400"
                   stroke="var(--base-100)"
                   strokeWidth={1.5}
                   cursor="pointer"
@@ -108,7 +129,7 @@ export default function MapChart() {
                   style={{
                     fill: "var(--base-100)",
                     fontSize: "0.75rem",
-                    fontWeight: 500,
+                    fontWeight: 600,
                   }}
                 >
                   {loc.name}
